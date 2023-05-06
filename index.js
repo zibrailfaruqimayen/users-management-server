6997209e3f4e61dc9e70f0fbea1bcb3d36f8766a
zibrailfaruqimayen <zibrail.faruqi@gmail.com>
2023-05-06 16:36:06 +0600

    data input from client site

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
 
+// middleware
+app.use(cors());
+app.use(express.json());
+
 const users = [
   { id: 1, name: "Sabana", email: "sabana@gmail.com" },
-  { id: 1, name: "Sabnoor", email: "sabnoor@gmail.com" },
-  { id: 1, name: "Sabila", email: "sabila@gmail.com" },
+  { id: 2, name: "Sabnoor", email: "sabnoor@gmail.com" },
+  { id: 3, name: "Sabila", email: "sabila@gmail.com" },
 ];
 
 app.get("/", (req, res) => {
@@ -14,6 +19,15 @@ app.get("/", (req, res) => {
 
 app.get("/users", (req, res) => {
   res.send(users);
+});
+
+app.post("/users", (req, res) => {
+  console.log("post api hitting");
+  console.log(req.body);
+  const newUser = req.body;
+  newUser.id = users.length + 1;
+  users.push(newUser);
+  res.send(newUser);
 });
 
 app.listen(port, () => {
